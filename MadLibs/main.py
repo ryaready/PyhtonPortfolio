5069956b5edf936baabf87cef351f40209d28fd6
ryaready <90432405+ryaready@users.noreply.github.com>
2021-11-30 16:36:18 +0800

    Add files via upload

---
+++ MadLibs/main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+adj = input("Adjective: ")
+name = input("Name: ")
+pronoun = input("Pronoun: ")
+verb1 = input("Verb:")
+noun = input("Noun:")
+verb2 = input("Verb: ")
+
+madLib = f"{name}'s mom is so {adj} when {pronoun} {verb1} the {noun} it {verb2}"
+
+print(madLib)
+
